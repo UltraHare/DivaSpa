a896bd9d9a59ff723c4bd44443ee443a2797733a
Oscar Enrique Rangel <60079588+UltraHare@users.noreply.github.com>
2025-03-09 14:26:13 -0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,86 @@
+<footer class="pt-4 bottom-0 container-fluid">
+    <div class="row p-5 pb-2 bg-dark">
+        <div class="col-xs-12 col-md-6 col-lg-3">
+            <h1 style="color: #bc5148;text-transform: uppercase;font-weight: 800;">Divas SPA</h1>
+            <p class="text-light">Somos variables, innovadores e inclusivos, focalizados en dar una experiencia única y
+                de calidad</p>
+        </div>
+        <div class="col-xs-12 col-md-6 col-lg-3">
+            <h3 class="text-light">Navegación</h3>
+            <div class="list-group">
+                <a href="#" class="list-group-item list-group-item-action bg-dark">
+                    ➤ Atención al cliente
+                </a>
+                <a href="#" class="list-group-item list-group-item-action bg-dark">
+                    ➤ Servicios
+                </a>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-md-6 col-lg-3">
+            <h5 class="text-light">Políticas y acuerdos</h5>
+            <div class="list-group">
+                <a href="terminos_condiciones.php" class="list-group-item list-group-item-action bg-dark">
+                    ➤ Términos y acuerdos
+                </a>
+                <a href="#" class="list-group-item list-group-item-action bg-dark">
+                    ➤ Políticas de privacidad
+                </a>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-md-6 col-lg-3">
+            <h5 class="text-light text-center mb-4">Contactanos</h5>
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        <a href="">
+                            <i class="fa fa-brands fa-instagram fa-2xl icon-color-social" style="color: #bc5148;"></i>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="">
+                            <i class="fa-brands fa-facebook-f fa-2xl icon-color-social" style="color: #bc5148;"></i>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="" class="text-light">
+                            <i class="fa-brands fa-whatsapp fa-2xl icon-color-social" style="color: #bc5148;"></i>
+                        </a>
+                    </div>
+                </div>
+                <h5 class="text-light text-center mt-4">
+                    Ubicación del local
+                </h5>
+                <p class="text-light">Gral J Velásquez 1993 1390000 Calama</p>
+            </div>
+        </div>
+        <div class="col-xs-12">
+            <p class="text-light text-center">© 2023 Todos los derechos reservados</p>
+        </div>
+    </div>
+</footer>
+
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<!-- Popper -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"
+    integrity="sha512-2rNj2KJ+D8s1ceNasTIex6z4HWyOnEYLVC3FigGOmyQCZc2eBXKgOxQmo3oKLHyfcj53uz4QMsRCWNbLd32Q1g=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- Bootstrap 5 -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
+<!-- OwlCarousel 2 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+    integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- FontAwesome -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js"
+    integrity="sha512-uKQ39gEGiyUJl4AI6L+ekBdGKpGw4xJ55+xyJG7YFlJokPNYegn9KwQ3P8A7aFQAUtUsAQHep+d/lrGqrbPIDQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="js/carousel.js"></script>
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es-cl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <!-- OwlCarousel 2 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+        integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+    <title>DivaSpa</title>
+</head>
+
+<body>
+    <header role="banner">
+        <nav class="navbar navbar-expand-lg fixed-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">Divas SPA</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto pl-lg-5 pl-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Ofertas
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Descuentos en servicios</a></li>
+                                <li><a class="dropdown-item" href="#">Promociones de productos</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Servicios
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="cortes.php">Cortes de cabello</a></li>
+                                <li><a class="dropdown-item" href="tratamiento_piel.php">Mantención y enbellecimiento de uñas</a></li>
+                                <li><a class="dropdown-item" href="#">Coloración de cabello</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sobre nosotros</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
